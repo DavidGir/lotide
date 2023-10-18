6c2a2959ea0e630a61d541f4291f13d5a4bb04d8
David Giroux <DavidGiroux149@gmail.com>
2023-10-18 10:12:13 -0400

    Add assertArraysEqual using eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,35 @@
+// Using function eqArrays for checking array equality and returns result:
+const eqArrays = function(array1, array2) {
+  // Check if both arrays have same length, if not return false.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  // For loop to iterate through elements of the first array.
+  for (let i = 0; i < array1.length; i++) {
+    // Compare both array indices
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  //Returns true if not false
+  return true;
+};
+
+// Creating function assertArraysEqual for handling output based on the result through call function:
+const assertArraysEqual = function(array1, array2) {
+  // Call function eqArrays with array1 and array2 as arguments:
+  let isMatch = eqArrays(array1, array2);
+
+  //Print results If isMatch true or false
+  if (isMatch === true) {
+    console.log("The arrays are equal.");
+  }
+
+  if (isMatch === false) {
+    console.log("The arrays are not equal.");
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass as equal
+assertArraysEqual([1, 2, 3], [1, 2, 5]); // SHould pass as not equal
