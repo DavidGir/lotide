7fb60978ed99f0cc206b0331480e922f8d67f72a
David Giroux <DavidGiroux149@gmail.com>
2023-10-18 23:01:52 -0400

    Added countOnly function

---
+++ countOnly.js
@@ -0,0 +1,59 @@
+// Function assertEqual is used to output assertion tests results:
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return; // Exit function if test passes.
+  }
+
+  console.error(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//----------------------------------------------------------------------------
+
+// Create function that should take in a collection of items and return counts for a specific subset of those items. It won't count everything. In order to decide what to count, it will also be given an idea of which items we care about and it will only count those, ignoring the others.
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  // Initialize variable meant to store counts as empty object
+  const results = {};
+  // For loop through names in allItems
+  for (const item of allItems) {
+    // Check if it is an item to count
+    if (itemsToCount[item]) {
+      // Check if item is in results
+      if (results[item]) {
+        // Increment the count:
+        results[item]++;
+      } else {
+        // Initialize the count as it is encountering it for first time:
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+// Assertion tests:
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
+
