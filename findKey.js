3ee5edbea51e260d984bac0aebfc84a0723106b8
David Giroux <DavidGiroux149@gmail.com>
2023-10-20 18:08:33 -0400

    made findKey.js

---
+++ findKey.js
@@ -0,0 +1,58 @@
+// Function implementation.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.error(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// ----------------------------------------------------------------------------
+
+// Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+const findKey = function(object, callback) {
+  const objectKeys = Object.keys(object);
+  for (const key of objectKeys) {
+    // Use callback function
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+
+// Input code:
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // Expected output to noma
+
+// Assertions tests:
+
+// Function should return first key where the value of stars is equal to 3:
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri"); // Expected output to pass
