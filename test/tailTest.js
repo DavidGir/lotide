b28f553519c69677ecfb7bd8dab7958b6ff3164e
David Giroux <DavidGiroux149@gmail.com>
2023-10-24 02:37:35 +0000

    Added assertion code using Mocha and Chai framework in this file.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,17 @@
 // test/tailTest.js
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello"]);
-assertEqual(result.length, 0); // Should pass; an array with only one element should yield an empty array for its tail.
-
-// Test Case 2:
-const result2 = tail([]);
-assertEqual(result2.length, 0); // Should pass; as an empty array should yield empty for its tail.
-
-// Test case 3 to check if tail function does not mutate the original array:
-const originalArray = ["Hello", "Lighthouse", "Labs"];
-const newArray = tail(originalArray);
-
-assertEqual(originalArray.length, 3);  // Original array should still have 3 elements.
-assertEqual(newArray.length, 2);  // New array should have 2 elements.
-
-// Test case 4 to check first element of the returned array from the tail function:
-assertEqual(newArray[0], "Lighthouse");  // First element of new array should be "Lighthouse".
+describe("#tail function", () => {
+  it("should return an empty array for an empty input array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("should return ['Lighthouse', 'Labs'] for an input array of ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("should have 'Lighthouse' as the first element of the new array", () => {
+    const inputArray = ["Hello", "Lighthouse", "Labs"];
+    const expectedArray = tail(inputArray);
+    assert.equal(expectedArray[0], "Lighthouse");
+  });
+});
