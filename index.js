a141916f040c17e6c44495b04a0d322a6991d5f5
David Giroux <DavidGiroux149@gmail.com>
2023-10-24 01:06:13 -0400

    Added object for mod exports and shorthand syntax

--- index.js
+++ index.js
@@ -17,23 +17,24 @@ const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
+//Apply property value shorthand syntax from ES6:
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
 
