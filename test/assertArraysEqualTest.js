5eba70e172b11dbb4f72409ffad64ae157b86e1a
David Giroux <DavidGiroux149@gmail.com>
2023-10-24 00:53:31 -0400

    Added assertion code for this test file.

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,14 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require("../assertArraysEqual");
 
 // Test code for assertArraysEqual:
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should pass as equal
-assertArraysEqual([1, 2, 3], [1, 2, 5]); // SHould pass as not equal
+describe("#assertArraysEqual", () => {
+
+  it("should return true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.deepEqual([1, 2, 3], [1, 2, 3]);
+  });
+
+  it("should return false for [1, 2, 3] and [1, 2, 5]", () => {
+    assert.notDeepEqual([1, 2, 3], [1, 2, 5]);
+  });
+});
