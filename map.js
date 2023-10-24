382005414d0294cc1ea011bf0bedaa8dd2b54a09
David Giroux <DavidGiroux149@gmail.com>
2023-10-24 00:39:57 -0400

    Implemented assertion code in mapTest.js.

--- map.js
+++ map.js
@@ -1,38 +1,5 @@
-// Using function eqArrays for checking array equality and returns result:
-const eqArrays = function(array1, array2) {
-  // Check if both arrays have same length, if not return false.
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  // For loop to iterate through elements of the first array.
-  for (let i = 0; i < array1.length; i++) {
-    // Compare both array indices
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  //Returns true if not false
-  return true;
-};
-
-// -----------------------------------------------------------------------------
-
-// Creating function assertArraysEqual for handling output based on the result through call function:
-const assertArraysEqual = function(array1, array2) {
-  // Call function eqArrays with array1 and array2 as arguments:
-  let isMatch = eqArrays(array1, array2);
-
-  //Print results If isMatch true or false
-  if (isMatch) {
-    console.log(`✅✅ The arrays ${array1} and ${array2} are equal.`);
-  }
-
-  if (!isMatch) {
-    console.log(`❌❌ The arrays ${array1} and ${array2} are not equal.`);
-  }
-};
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // -----------------------------------------------------------------------------
 
@@ -58,9 +25,4 @@ const results1 = map(words, word => word[0]);
 
 console.log(results1);
 
-// Assertion tests:
-
-assertArraysEqual(map(["cat", "dioji", "bird"], word => word.length), [3, 5, 4]);  // Should pass
-assertArraysEqual(map([1, 2, 3], num => num * 2), [2, 4, 6]);  // Should pass
-assertArraysEqual(map(["a", "b", "c"], char => char.toUpperCase()), ["A", "B", "C"]);  // Should pass
-
+module.exports = map;
