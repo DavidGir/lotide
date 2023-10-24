791571d9adcc84ff468f7ea20763a8d091e3955d
David Giroux <DavidGiroux149@gmail.com>
2023-10-24 02:48:03 +0000

    Added assertion code to middleTest.js using Mocha and Chai frameworks.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// Assertion tests for the middle function:
-assertArraysEqual(middle([1]), []); // Should pass
-assertArraysEqual(middle([1, 2]), []); // Should pass
-assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Should pass
+
+describe("#middle function", () => {
+  it("should return an empty array for an input array of [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should return an empty array for an input array of [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("should return the middle element [2] for an input array of [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("should return the middle elements [2, 3] for an input array of [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return the middle element ['b'] for an input array of ['a', 'b', 'c']", () => {
+    assert.deepEqual(middle(["a", "b", "c"]), ["b"]);
+  });
+});
